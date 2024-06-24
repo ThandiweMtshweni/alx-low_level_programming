4952c1ed2bc463dba9c099fcf1282bd9a25283ba
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-24 09:40:47 -0700

    Print base of 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main (void)
+{
+	char num;
+
+	for (num = '0'; num < 10; num ++)
+	{
+		putchar(num);
+	}
+	for (num = 'a'; num < 'g'; num ++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+
+}
