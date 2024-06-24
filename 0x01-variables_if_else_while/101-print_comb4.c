1017144fdc14cb3e9f4df273784e9e6d9053cd96
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-24 14:58:33 -0700

    More combos

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int first = '0';
+	int sec = '0';
+	int third = '0';
+
+	for (third = '0'; third <= '9'; third++)
+	{
+		for (sec = '0'; sec <= 9; sec++)
+		{
+			for (first = '0'; first <= '9'; first++)
+			{
+				if (!((first == sec) || (sec == third) || (sec > first) || (third > sec)))
+				{
+					putchar(third);
+					putchar(sec);
+					putchar(first);
+					if (!(first == '9' && third == '7' && sec == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					
+					}
+				}
+			}
+		}
+	}
+	return (0);
+}
