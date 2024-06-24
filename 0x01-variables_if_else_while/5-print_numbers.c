b6cb57c53dedbb7c10f095dec857a2c7c3e1342c
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-24 04:30:59 -0700

    Print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: always 0 (success)
+ *
+ */
+int main(void)
+{
+	int num = 0;
+
+	for (num = 0; num < 10; num ++)
+	{
+	 printf("%d",num);
+	}
+
+	printf("\n");
+	return (0);
+}
