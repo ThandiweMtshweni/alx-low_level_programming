bbd1b7363fedce749bbb5654a4048a558d80ab74
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-24 02:49:55 -0700

    alphaBET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char lower_case = 'a';
+	char upper_case = 'A';
+	int i;
+	for (i = 0; lower_case < 26; i++)
+	{
+		putchar(lower_case + i);
+	}
+	for (i = 0; upper_case < 26; i++)
+	{
+		putchar(upper_case + i);
+	}
+	putchar('\n');
+	return (0);
+}
