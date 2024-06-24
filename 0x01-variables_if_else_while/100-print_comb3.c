627fddb143c992e4c01d41739d19e9854f55fe1f
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-24 14:02:33 -0700

    Combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,32 +5,34 @@
  * Return: Always 0 (success)
  */
 int main(void)
-{	
-	int first_digit = '0';
-	int sec_digit = '0';
-	
-	while(first_digit <= '9')
+{
+	int num1 = '0';
+	int num2 ='0';
+
+	while(num1 <= '9')
 	{
-		while(sec_digit <= '9')
+		while (num2 <= '9')
 		{
-			if (!(first_digit > sec_digit) || first_digit == sec_digit )
+			if (!(num1 > num2) || num1 == num2)
 			{
-				putchar(first_digit);
-				putchar(sec_digit);
-				if (first_digit == '8' && sec_digit == '9')
-					{
-						putchar('\n');	
-					}
-					else
-					{
-						putchar(',');
-						putchar(' ');
-					}
+				putchar(num1);
+				putchar(num2);
+				if (num1 == '8' && num2 == '9')
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
-			sec_digit++;
+			num2++;
 		}
-		sec_digit = '0';
-		first_digit++;
+		num2 = '0';
+		num1++;
+	
 	}
-return (0);
+	return (0);
+
 }
