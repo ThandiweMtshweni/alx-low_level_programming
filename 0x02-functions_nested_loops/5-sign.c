e4cc0a4a92dad631711aa2cb826211ba76609292
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-25 14:14:21 -0700

    Print signs

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * int print_sign - print the sign of a number
+ * print_sign - Print the sign of a number
  * @n: The number to check
  * Return: 1 for positive number, -1 for negative number and 0 for other
  */
 
 int print_sign(int n)
 {
-	if(n > 0)
+	if (n > 0)
 	{
 	_putchar(43);
 	return (1);
