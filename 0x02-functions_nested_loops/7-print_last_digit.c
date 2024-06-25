7fdd8a93eed1cf153e3ce52fed2e1c7a07f4963c
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-25 14:35:11 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_last_digit - prints the last digit 
+ * @num: The number to be printed
+ * Return: The value of the last digit
+ */
+
+int print_last_digit(int num)
+{
+	int last_digit;
+
+	last_digit = num % 10;
+	if (last_digit < 0)
+	{
+	last_digit = last_digit * -1;
+	}
+
+	_putchar(last_digit * '0');
+	return (last_digit);
+}
