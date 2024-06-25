a2a7a94ffe626f50acb5b521139464c4d64a3f95
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-25 10:55:52 -0700

    Print alphabets 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print alphabets 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	char i;
+	
+	for (x = 1; x <= 10; x++)
+	{
+		for (i = 'a'; i <= 'z'; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
