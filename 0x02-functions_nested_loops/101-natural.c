c1a4b6114105835ae43b880157c4a276d29e2234
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-25 16:52:20 -0700

    multiples of 3 and 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int x;
+	int i = 0;
+
+	while (x < 1024)
+	{
+	if ((x % 3 == 0) || (x % 5 == 0))
+	{
+	i += x;
+	}
+	printf("%d\n", z);
+	return (0);
+	}
+}
