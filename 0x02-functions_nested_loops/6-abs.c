0fe11c70146f6590417dabb7a020a9946793d411
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-25 14:22:56 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of and int
+ * @i: The number to compute
+ * Return: The absolute value of a number
+ */
+
+int _abs(int i)
+{
+	if (i < 0)
+	{
+	int abs_value;
+	abs_value = i * -1;
+	return (abs_value);
+	}
+	return (i);
+}
