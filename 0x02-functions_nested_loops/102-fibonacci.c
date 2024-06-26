57bdec641146a5ec9ccad9574c5621432600d071
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-25 17:18:41 -0700

    first 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i = 0;
+	long x = 1; z = 2;
+
+	while (i < 50)
+	{
+	if (i == 0)
+	printf("%d", x);
+	else if (i == 1)
+	printf(", %d", z);
+	else
+	{
+	z += x;
+	x = z - x;
+	printf(", %d", k);
+	}
+	i++;
+	}
+	printf("\n");
+	return (0);
+}
