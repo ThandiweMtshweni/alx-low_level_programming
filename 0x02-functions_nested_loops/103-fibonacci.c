8af7706c5d6e40dec69e14b7d05d16309e9b354c
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-25 17:33:31 -0700

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print sum of even fibonacci numbers
+ * Return: void
+ */
+
+int main(void)
+{
+	int i = 0;
+	long x = 1, z = 2, sum = z;
+
+	while (z + x < 4000000)
+	{
+	z += z;
+	if (z % 2 == 0)
+	sum += z;
+	x = z - x;
+	i++;
+	}
+	printf("%ld\n", sum);
+	return (0);
+
+}
