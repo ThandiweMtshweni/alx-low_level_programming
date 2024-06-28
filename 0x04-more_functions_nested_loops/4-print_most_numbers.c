a6e7877217d41ed1e8f426584d9dfe9e0aeafbef
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-28 04:22:09 -0700

    Print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,15 @@
  */
 
 void print_most_numbers(void)
-
+{
 char i;
 
-for (i = '0'; i <= '9'; i++)
-{
-if !(i == '2' && i == '4')
-{
-_putchar(i);
-}
-_putchar('\n');
+	for (i = '0'; i <= '9'; i++)
+	{
+	if (!(i == '2' && i == '4'))
+	{
+	_putchar(i);
+	}
+	_putchar('\n');
+	}
 }
