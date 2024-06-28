a4e7c46b681490bd2fe1de6d047878d4faf5bfb1
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-06-28 06:05:40 -0700

    Print a line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_line - Draws a line
+ * @n: determines how long the line will be
+ * Return: Always 0
+ * 
+ */
+
+void print_line(int n)
+{
+int i;
+
+for (i = 0; i <= n; i++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
