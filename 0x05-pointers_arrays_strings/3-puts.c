3af860c5a7724b2a7ceff0377f25842be65eae3d
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-07-01 11:14:58 -0700

    I do not fear computers, I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - Prints a string followed by a new line
+ * @str:- The string to be printed
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
