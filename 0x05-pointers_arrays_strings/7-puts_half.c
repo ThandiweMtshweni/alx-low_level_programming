a423f141b5c73e5f85d127c4a8a78b946d315dbb
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-07-01 15:45:07 -0700

    Prints half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - Prints half of a string
+ * @str - The string to be printed
+ * Return: Void
+ */
+
+void puts_half(char *str)
+{
+	int full_str, half_str;
+
+	full_str = 0;
+	while (str[full_str] != '\0')
+		full_str++;
+
+	half_str = full_str / 2;
+
+	if (full_str % 2 == 1)
+		half_str++;
+
+	while (half_str < full_str)
+	{
+		_putchar(str[half_str]);
+		half_str++;
+	}
+	_putchar('\n');
+}
