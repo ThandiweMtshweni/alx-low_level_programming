9041d03fca26bbe61e40477d161db3181fc544b4
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-07-04 06:02:22 -0700

    Concatinate two strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strncat - Concatenates two strings
+ * @dest: destination string pointer
+ * @src: source string pointer
+ * @n: the number of bytes to be concatenated
+ * Return: The pointer to destination string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int length, i;
+
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (i = 0; i < n && src[i] != '\n'; i++, length++)
+	{
+		dest[length] = src[i];
+	}
+	dest[length] = '\0';
+	return (dest);
+}
