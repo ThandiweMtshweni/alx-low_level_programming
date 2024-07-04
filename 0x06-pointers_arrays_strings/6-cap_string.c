1d4e879096e1d848fdd707c34fc0af01a6a251ce
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-07-04 13:33:28 -0700

    Cat string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,11 @@ char *cap_string(char *s)
 		{
 			s[0] = s[0] - 32;
 		}
-		if (s[str_count] == ' ' || s[str_count] == '\t' || s[str_count] == '\n' || s[str_count] == ',' || s[str_count] == ';' || s[str_count] == '.' || s[str_count] == '!' || s[str_count] == '?' || s[str_count] == '"' || s[str_count] == '(' || s[str_count] == ')' || s[str_count] == '{' || s[str_count] == '}')
+		if (s[str_count] == ' ' || s[str_count] == '\t' || s[str_count] == '\n'
+			|| s[str_count] == ',' || s[str_count] == ';' || s[str_count] == '.'
+			|| s[str_count] == '!' || s[str_count] == '?' || s[str_count] == '"'
+			|| s[str_count] == '(' || s[str_count] == ')' || s[str_count] == '{'
+			|| s[str_count] == '}')
 		{
 			if (s[str_count + 1] >= 97 && s[str_count + 1] <= 122)
 			{
