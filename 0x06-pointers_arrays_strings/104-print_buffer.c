5b465e395ec7d6485d55e2cfd0766fc1af84ac34
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-07-05 12:07:51 -0700

    print a buffer

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_buffer - printd buffer
+ * @b: the buffer
+ * @size: the size
+ * Return: void
+ */
+
+void print_buffer(char *b, int size)
+{
+}
