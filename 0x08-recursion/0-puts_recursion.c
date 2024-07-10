d572ceadad698f95843c6bdb98842fa094bc9fde
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-07-10 08:14:54 -0700

    Puts with recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a recursive function to print a string
+ * @s: pointer to the string
+ * Return: void
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	if (*s == '\0')
+	{
+		_putchar('\n');
+	}
+}
