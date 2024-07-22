36c7976c2211450f6b94179f640e221b85e1508c
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-07-22 12:21:26 -0700

    A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,6 +2,7 @@
 #define MY_DOG_H
 
 /**
+ *
  * struct dog - defines new type
  * @name: The name of the dog
  * @age: The age of the dog
@@ -15,4 +16,6 @@ struct dog
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 #endif
