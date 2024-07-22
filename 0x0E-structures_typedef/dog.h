464d58a6d32cfb934a952ad9601ed0ded25555a7
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-07-22 11:54:27 -0700

    Learning about struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef MY_DOG_H
+#define MY_DOG_H
+
+/**
+ * struct dog - defines new type
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: Dog owner's name
+ * Description: my first struct
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
