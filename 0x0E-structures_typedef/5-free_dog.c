1bf56656257cdd09593809e3822b75c84614586b
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-07-23 02:52:43 -0700

    free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function frees dogs
+ * @d: pointer to the dog
+ * Return: void
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+	}
+}
