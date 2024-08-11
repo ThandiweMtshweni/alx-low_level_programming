9beffb80c8ac257b279d0c106d1aa1f5c3917b55
ThandiweMtshweni <thandiwemtshweni@gmail.com>
2024-08-11 03:46:37 -0700

    Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef MY_DOG_H
-#define MY_DOG_H
+#ifndef DOG_H
+#define DOG_H
 
 /**
  * struct dog - defines new type
@@ -9,17 +9,19 @@
  * Description: my first struct
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 
 #endif
-
